e9cd6ac1301a25ef88b015740da871baab88127b
ZZ22 <zz22@ZZ22s-MBP.fritz.box>
2020-11-13 21:39:39 +0100

    deleted remaining files

--- Application/t1.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import numpy as np
-
-def app(seed,sidebar_top):
-    pi = 3
-    
--- Application/t2.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import numpy as np
-from numpy.random import default_rng
-
-def app(seed,sidebar_top):
-    pi = 3
